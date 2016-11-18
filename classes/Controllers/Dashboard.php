aa547fa31f2683831f80f33f1bc4c3c7367e9715
Mauirce Moss <cammygamesyt@gmail.com>
2016-11-18 16:19:04 +0000

    Added login view.

---
+++ classes/Controllers/Dashboard.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is from Slimworks and developed by Cyberbyte Studios
+ */
+
+namespace Slimworks\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+// use Slim\Interfaces\RouterInterface as Router;
+use Slimworks\Interfaces\ViewInterface as View;
+use Slimworks\Helpers\RenderView;
+// use Noodlehaus\ConfigInterface as Config;
+// use Slimworks\Interfaces\Helpers\SessionInterface as Session;
+use Slimworks\Interfaces\Database\DatabaseInterface as DB;
+use Slimworks\Interfaces\Helpers\FlashInterface as Flash;
+// use Psr\Log\LoggerInterface as Logger;
+
+/**
+ * GetHome Controller
+ *
+ * @param  \Psr\Http\Message\ServerRequestInterface $request    PSR7 request
+ * @param  \Psr\Http\Message\ResponseInterface      $response   PSR7 response
+ * @param  App\Interfaces\ViewInterface             $view       e.g. Slim\Views\Twig
+ * @param  App\Interfaces\FlashInterface            $flash      e.g. Slim\Flash\Messages
+ *
+ * @return HTTP response message
+ *
+ */
+class Dashboard
+{
+    public function __invoke(
+        Request $request,
+        Response $response,
+        View $view,
+        DB $db,
+        Flash $flash
+    ) {
+        return new RenderView($request, $response, $view, 'life/dashboard/content', [
+            'someParam' => 'nothing was returned from the db.',
+        ]);
+    }
+}
